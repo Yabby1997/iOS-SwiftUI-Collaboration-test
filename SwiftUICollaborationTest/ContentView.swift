4b826458ab7689f60c32b6cf40f4108f95fce68b
yabby <yabby1997@gmail.com>
2021-05-05 20:44:00 +0900

    TrafficLight 기능 구현

--- SwiftUICollaborationTest/ContentView.swift
+++ SwiftUICollaborationTest/ContentView.swift
@@ -7,14 +7,44 @@
 
 import SwiftUI
 
+extension Color {
+    mutating func nextTrafficLight() {
+        switch(self) {
+        case .green:
+            self = .yellow
+        case .yellow:
+            self = .red
+        case . red:
+            self = .green
+        default:
+            self = .green
+        }
+    }
+}
+
 struct ContentView: View {
+    // MARK: - Properties
+    
+    @State var trafficLight: Color = .green
+    
+    // MARK: - Lifecycles
+    
     var body: some View {
         NavigationView {
-            NavigationLink(
-                destination: CounterView(),
-                label: {
-                    Text("Navigate to Counter")
-                })
+            ZStack {
+                self.trafficLight
+                    .edgesIgnoringSafeArea(.all)
+                VStack(alignment: .center, spacing: 8){
+                    Button("Traffic Light") {
+                        self.trafficLight.nextTrafficLight()
+                    }
+                    NavigationLink(
+                        destination: CounterView(),
+                        label: {
+                            Text("Navigate to Counter")
+                        })
+                }
+            }
         }
     }
 }
