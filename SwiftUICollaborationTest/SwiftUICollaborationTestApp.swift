13ccd4dea90ddc1abe67b4f0abbc7b63bc2b1baf
yabby <yabby1997@gmail.com>
2021-05-05 20:10:53 +0900

    Initial Commit

---
+++ SwiftUICollaborationTest/SwiftUICollaborationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICollaborationTestApp.swift
+//  SwiftUICollaborationTest
+//
+//  Created by Seunghun Yang on 2021/05/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICollaborationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
